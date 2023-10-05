be4e6ce547ab679125772816df0fbeeb09e9abee
u201334 <u201334@login-2.aidevcloud>
2023-10-05 01:40:59 -0700

    Upload

--- BubbleSortParallel.c
+++ BubbleSortParallel.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <omp.h>
 
-// function to calculate the nth Fibonacci number recursively
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    } else {
-        int x, y;
-        #pragma omp task shared(x) firstprivate(n)
-        x = fibonacci(n - 1);
+void parallelBubbleSort(int arr[], int n) {
+    int temp;
+    int swapped;
 
-        #pragma omp task shared(y) firstprivate(n)
-        y = fibonacci(n - 2);
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0; // adding a flag variable to optimize the loop
 
-        #pragma omp taskwait
-        return x + y;
+        #pragma omp parallel for shared(arr, swapped)
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) { // swapping arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1; // changing the flag if a swap occurred
+            }
+        }
+        // the array is in the sorted form when no values swapped
+        if (swapped == 0) {
+            break;
+        }
     }
 }
 
 int main() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int result;
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     double tstart, tstop, tcalc; // Variables for time measurement
 
     tstart = omp_get_wtime(); // Record start time
 
-    #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            result = fibonacci(n);
-        }
-    }
+    parallelBubbleSort(arr, n);
 
     tstop = omp_get_wtime(); // Record stop time
     tcalc = tstop - tstart; // Calculate time taken
 
-    printf("Fibonacci(%d) = %d\n", n, result);
-    printf("Time taken: %.2f seconds\n", tcalc); // Print time taken
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nTime taken: %.2f seconds\n", tcalc); // Print time taken
 
     return 0;
 }
-
